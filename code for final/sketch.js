8d045c46ce355f395b57a449dcd0a8f5fcb39ce7
kimtaemin213 <57379125+kimtaemin213@users.noreply.github.com>
2019-12-02 14:45:50 +0900

    createCanvas

---
+++ code for final/sketch.js
@@ -0,0 +1,18 @@
+
+let canvasWidth =1280;
+let canvasHeight=800;
+
+function setup()
+{
+  canvas = createCanvas(canvasWidth, canvasHeight);
+  canvas.position(windowWidth/2 - canvasWidth/2, 20);
+
+}
+
+function draw()
+{
+  background(0);
+
+
+}
+
