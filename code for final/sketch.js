e2541020629af143ff298e298661e5be9d9c628b
kimtaemin213 <kimtaemin@DESKTOP-HU6PND5.localdomain>
2019-12-04 02:45:38 +0900

    sprites

--- code for final/sketch.js
+++ code for final/sketch.js
@@ -1,56 +1,33 @@
 
 let canvasWidth =1280;
 let canvasHeight=800;
-let character_X = 50;
-let character_Y = 50;
-let character_posX = 0;
-let character_posY = 0;
-let obj_posX = 0;
-let obj_posY = 0;
-let obj_X = 50;
-let obj_Y = 50;
-let rect_posx;
-let rect_posy;
+let character,obj;
+let character_posX,character_posY;
+let obj_posX,obj_posY;
 
-
-
-function setup()
-{
+function setup() {
   canvas = createCanvas(canvasWidth, canvasHeight);
   canvas.position(windowWidth/2 - canvasWidth/2, 20);
-  rect_posx = random(0,1280);
-  rect_posy = random(0,800);
+  character_posX =random(300,900);
+  character_posY =random(0,height/2);
+  obj_posX = random(0,1280);
+  obj_posY = random(0,800);
 
-
-}
-
-function draw()
-{
-  background(0);
-  fill(255,255,0);
-  rect(character_posX,character_posY,character_X,character_Y);
-
-  fill(0,255,125);
-  rect(rect_posx,rect_posy,obj_X,obj_Y);
-
-
+  show();
 
 }
 
-function keyPressed() {
-  if(keyIsDown(LEFT_ARROW)){
-    character_posX = character_posX -10;
-  }
-  if(keyIsDown(RIGHT_ARROW)){
-    character_posX = character_posX +10;
-  }
-  if(keyIsDown(UP_ARROW)){
-    character_posY = character_posY -10;
-  }
-  if(keyIsDown(DOWN_ARROW)){
-    character_posY = character_posY +10;
-  }
-  
+function draw() {
+background(255);
+drawSprites();
 
 
 }
+function show() {
+  character =createSprite(character_posX,character_posY,50,50);
+  character.shapeColor="black";
+  obj =createSprite(obj_posX,obj_posY,50,50);
+
+}
+
+
